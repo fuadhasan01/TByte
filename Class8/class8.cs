582338fd3f86be0af0bd2d8e019c11af9bae4e9f
Fuad Hasan Labib <labib.knc@gmail.com>
2023-05-25 00:35:16 +0600

    Class 8

---
+++ Class8/class8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+
+
+
+    public class StringManipulation
+    {
+        static void Main(String[] args)
+        {
+            int myAge = 42;
+            string myName = "Fuad";
+            string myJob = "Student";
+            Console.WriteLine("String Concatenation");
+            Console.WriteLine("Hey! It is " + myName + " . I am " + myAge + " I am a " + myJob);
+
+            Console.WriteLine("String Indexing Format");
+            Console.WriteLine("Hey! It is {0} . I am {1} I am a {2}", myName, myAge, myJob);
+
+            Console.WriteLine("String Interpolation");
+            Console.WriteLine("Hey! It is {myName} . I am {myAge} I am a {myJob}");
+        }
+    }
+}
